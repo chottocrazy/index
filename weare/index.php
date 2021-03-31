094c0c1bd515c1c6885de950b7322937ba21bede
Very Gois <54934777+kazumasasajima@users.noreply.github.com>
2021-04-01 01:00:53 +0900

    Update index.php

--- weare/index.php
+++ weare/index.php
@@ -33,7 +33,7 @@ fclose($fp);
 <title>大 chotto crazy by Pehu</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="http://creative-community.pe.hu/coding/js/org.js"></script>
-<link rel="stylesheet" href="/css/programs.css" />
+<link rel="stylesheet" href="/css/about.css" />
 <style type="text/css">
 #header b,
 .hold:before,
