f1ee3df5692af57204b629ab6436c1c6e4766ad4
Very Gois <54934777+kazumasasajima@users.noreply.github.com>
2021-04-01 01:01:56 +0900

    Update index.php

--- programs/index.php
+++ programs/index.php
@@ -36,7 +36,7 @@ fclose($fp);
 <title>公式プログラム一覧 | 大 chotto crazy</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="/js/programs.js"></script>
-<link rel="stylesheet" href="/css/programs.css" />
+<link rel="stylesheet" href="/css/about.css" />
 <style type="text/css">
 .hold:before,
 .on {
