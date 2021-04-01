93285fee35564536228798089ea8e1fac1baa92d
Very Gois <54934777+kazumasasajima@users.noreply.github.com>
2021-04-01 14:26:40 +0900

    Update about.php

--- online/map/okayama/about.php
+++ online/map/okayama/about.php
@@ -6,7 +6,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="ramdom.js"></script>
 <title>岡山県オススメ60スポット | イトウヒロキ</title>
-<link rel="stylesheet" type="text/css" href="/css/about.css" />
+<link rel="stylesheet" type="text/css" href="/css/programs.css" />
 <style type="text/css">
 .about_map #sub {color:orange;}
 .about_map u,
