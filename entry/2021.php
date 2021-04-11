1601902c34da75f0b0517ff815723c1f651aa4d1
Very Gois <54934777+kazumasasajima@users.noreply.github.com>
2021-04-11 18:01:16 +0900

    Update 2021.php

--- entry/2021.php
+++ entry/2021.php
@@ -6,17 +6,15 @@ function h($str) {
 
 $content = (string)filter_input(INPUT_POST, 'content'); // $_POST['content']
 $name = (string)filter_input(INPUT_POST, 'name'); // $_POST['name']
-$email = (string)filter_input(INPUT_POST, 'email'); // $_POST['email']
 $when = (string)filter_input(INPUT_POST, 'when'); // $_POST['when']
 $where = (string)filter_input(INPUT_POST, 'where'); // $_POST['where']
 $why = (string)filter_input(INPUT_POST, 'why'); // $_POST['why']
 $how = (string)filter_input(INPUT_POST, 'how'); // $_POST['how']
-$public = (string)filter_input(INPUT_POST, 'public'); // $_POST['public']
 
 $fp = fopen('2021.csv', 'a+b');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     flock($fp, LOCK_EX);
-    fputcsv($fp, [$content, $name, $email, $when, $where, $why, $how, $public ]);
+    fputcsv($fp, [$content, $name, $when, $where, $why, $how ]);
     rewind($fp);
 }
 
@@ -43,17 +41,19 @@ fclose($fp);
 <ul id="public">
 <?php if (!empty($rows)): ?>
 <?php foreach ($rows as $row): ?>
-<li id="<?=h($row[7])?>">
+<li id="yes">
 <div>
 <p id="bg_000" class="title">
 <span><?=h($row[0])?></span></p>
-<span class="who">by <i><?=h($row[1])?></i></span>
 <div class="plan">
-<b class="date"><?=h($row[3])?></b>
-<b class="where"><?=h($row[4])?></b>
-<p class="why"><?=h($row[5])?></p>
-<p class="why"><?=h($row[6])?></p>
+<b class="date"><?=h($row[2])?></b>
+<b class="where"><?=h($row[3])?> で</b>
+<p class="why"><?=h($row[4])?><br/>
+<span class="who">by <i><?=h($row[1])?></i></span>
+</p>
 </div>
+<a href="<?=h($row[5])?>" target="_blank" rel="noopener noreferrer"></a>
+<img class="pdf" src="http://vg.pe.hu/merchandise/icon/pdf.png">
 </div>
 </li>
 <?php endforeach; ?>
@@ -61,16 +61,34 @@ fclose($fp);
 <li id="yes">
 <div>
 <p id="bg_000" class="title">
-<span>応募内容</span></p>
-<span class="who">by <i>名前</i></span>
+<span><?=h($row[0])?></span></p>
 <div class="plan">
-<b class="date">とき</b>
-<b class="where">ところ</b>
-<p class="why">なぜ</p>
+<b class="date"><?=h($row[2])?></b>
+<b class="where"><?=h($row[3])?> で</b>
+<p class="why"><?=h($row[4])?><br/>
+<span class="who">by <i><?=h($row[1])?></i></span>
+</p>
 </div>
+<a href="<?=h($row[5])?>" target="_blank" rel="noopener noreferrer"></a>
+<img class="pdf" src="http://vg.pe.hu/merchandise/icon/pdf.png">
 </div>
 </li>
 <?php endif; ?>
+<li id="yes">
+<div>
+<p id="bg_000" class="title">
+<span>ここでなら自分のやりたいことができるかもしれないって思える場所を作りたい</span></p>
+<div class="plan">
+<b class="date">2021年12月31日まで に</b>
+<b class="where">音ビル／ウェブサイト で</b>
+<p class="why">世間・世論がルールで、社会秩序を守ることが正義みたいな雰囲気が嫌だ。みんながどうとか、人がこう言ってるとか、そういうしがらみがない環境にいたい。<br/>
+<span class="who">by <i>creative, community space ∧°┐</i></span>
+</p>
+</div>
+<a href="___" target="_blank" rel="noopener noreferrer"></a>
+<img class="pdf" src="http://vg.pe.hu/merchandise/icon/pdf.png">
+</div>
+</li>
 </ul>
 </div>
 <script type="text/javascript">
