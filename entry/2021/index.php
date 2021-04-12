0e43624fabeab2138859c2b94556bae12a634da2
Very Gois <54934777+kazumasasajima@users.noreply.github.com>
2021-04-12 11:03:06 +0900

    Update index.php

--- entry/2021/index.php
+++ entry/2021/index.php
@@ -32,6 +32,7 @@ fclose($fp);
 <meta name="viewport" content="width=device-width">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <link href="/css/plan.css" rel="stylesheet">
+<link href="/css/about.css" rel="stylesheet">
 <title>大 chotto crazy 2021 | We Support your Amazing Ideas and Creative Challenges</title>
 <style type="text/css">
 .on {font-family:"Orchard";}
