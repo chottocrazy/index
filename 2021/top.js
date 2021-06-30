8a9e9cc794df413fb7fa2e3b3efea6f65ac7824d
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-01 03:17:54 +0900

    2021

---
+++ 2021/top.js
@@ -0,0 +1,36 @@
+var str = "";
+var sentences = str.split(".");
+var lastScrollTop = 0;
+var reverse = false;
+
+var synth = new Tone.Synth().toMaster();
+var autoFilter = new Tone.AutoFilter({
+}).connect(Tone.Master);
+
+autoFilter.start()
+StartAudioContext(Tone.context, 'div');
+//have to click to start audio context
+$('div').click(function(){
+  Tone.start();
+});
+
+$(window).scroll(function(event){
+  var st = $(this).scrollTop();
+  console.log(st);
+  if (st > lastScrollTop){
+  //scrolled down
+synth.triggerAttackRelease("C4", 2);
+  } else {
+  //scrolled up
+    synth.triggerAttackRelease("G4", 2);
+  }
+  lastScrollTop = st;
+
+});
+
+
+
+$(window).resize(function(){
+  w = $(window).width();
+  h = $(window).height();
+});
