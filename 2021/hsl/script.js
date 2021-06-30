74bea07b408705cfd354dd974cd3cd3047825cbd
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-01 06:29:10 +0900

    melody

--- 2021/hsl/script.js
+++ 2021/hsl/script.js
@@ -10,7 +10,7 @@ $(document).on('mousemove', function(e){
     var sraw = parseInt(100 - Math.round((e.pageX + 0.1) / ($(window).width()) * 100));
       var lraw = parseInt(Math.round((e.pageX + 0.1) / ($(window).width()) * 100));
       $('#color').css({'background': 'hsl(' + hueraw + ',' + sraw + '%,' + lraw + '%)'})
-      $('#color, #rolling').css({'color': 'hsl(' + hueraw + ',' + sraw + '%,' + lraw + '%)'})
+      $('#color, #rolling, .innertext').css({'color': 'hsl(' + hueraw + ',' + sraw + '%,' + lraw + '%)'})
       $('#saturationcount').text(sraw + '%');
       $('#lightnesscount').text(lraw + '%');
   }
