fda14fa46330faa0478c5d1dbc8d2044e1efed5b
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-02 08:31:34 +0900

    Update click.js

--- 2021/online/tone/click.js
+++ 2021/online/tone/click.js
@@ -3,14 +3,13 @@ $(window).click(function(){
     Tone.context.resume();
 });
 
-var synth = new Tone.PolySynth(5, Tone.Synth).toMaster();
+var synth = new Tone.AMSynth(5, Tone.Synth).toMaster();
 var notes = Tone.Frequency("E3").harmonize([0, 2, 5, 7, 9, 12,
-                                            3, 6, 8, 10, 13,
-                                            6, 8, 10, 13, 15,
-                                            8, 10, 13, 15, 18,
-                                            10, 13, 15, 18, 20,
-                                            13, 15, 18, 20, 22,
-                                            15, 18, 20, 22, 25]);
+                                            2, 5, 7, 9, 12, 15,
+                                            5, 7, 9, 12, 15, 17,
+                                            7, 9, 12, 15, 17, 20,
+                                            9, 12, 15, 17, 20, 22,
+                                            12, 15, 17, 20, 22, 25]);
 var noteIndex = 0;
 
 $("body").click(function(e){
