fb3d8fb31980cf435e6f8d72461aae6bc121a6c7
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-02 08:32:47 +0900

    Update click.js

--- 2021/online/tone/click.js
+++ 2021/online/tone/click.js
@@ -4,10 +4,7 @@ $(window).click(function(){
 });
 
 var synth = new Tone.AMSynth(5, Tone.Synth).toMaster();
-var notes = Tone.Frequency("E3").harmonize([0, 2, 5, 7, 9, 12,
-                                            2, 5, 7, 9, 12, 15,
-                                            5, 7, 9, 12, 15, 17,
-                                            7, 9, 12, 15, 17, 20,
+var notes = Tone.Frequency("E3").harmonize([7, 9, 12, 15, 17, 20,
                                             9, 12, 15, 17, 20, 22,
                                             12, 15, 17, 20, 22, 25]);
 var noteIndex = 0;
