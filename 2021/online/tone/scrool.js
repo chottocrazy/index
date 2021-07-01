e96b26b1d7b84663986ac3b87205e52e072a6e5c
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-02 01:56:54 +0900

    tone

--- 2021/online/tone/scrool.js
+++ 2021/online/tone/scrool.js
@@ -4,6 +4,20 @@ var lastScrollTop = 0;
 var reverse = false;
 
 var synth = new Tone.AMSynth().toMaster();
+var notes = Tone.Frequency("G2").harmonize([1, 3, 6, 8, 10,
+                                            3, 6, 8, 10, 13,
+                                            6, 8, 10, 13, 15,
+                                            8, 10, 13, 15, 18,
+                                            10, 13, 15, 18, 20,
+                                            13, 15, 18, 20, 22,
+                                            15, 18, 20, 22, 25,
+                                            18, 20, 22, 25, 27,
+                                            20, 22, 25, 27, 30,
+                                            22, 25, 27, 30, 32,
+                                            25, 27, 30, 32, 34,
+                                            27, 30, 32, 34, 37]);
+var noteIndex = 0;
+
 var autoFilter = new Tone.AutoFilter({
 }).connect(Tone.Master);
 
@@ -19,12 +33,13 @@ $(window).scroll(function(event){
   console.log(st);
   if (st > lastScrollTop){
   //scrolled down
-synth.triggerAttackRelease("C4", 1);
+synth.triggerAttackRelease(notes[noteIndex], "8n");
   } else {
   //scrolled up
-    synth.triggerAttackRelease("G4", 1);
+synth.triggerAttackRelease(notes[noteIndex], "8n");
   }
   lastScrollTop = st;
+
 
 });
 
