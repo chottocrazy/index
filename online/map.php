23d863ae051e5e61be3b180c860f3c8e88755eab
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-03 20:06:21 +0900

    Update map.php

--- online/map.php
+++ online/map.php
@@ -29,7 +29,7 @@ $("#howtocoding").load("/online/howtocoding.html");
 }
 #sign  {
   position: relative;
-  width:100%; height:23.5rem;
+  width:100%; height:100%;
   border:none;
   overflow:hidden;
 }
