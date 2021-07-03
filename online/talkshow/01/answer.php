b393aa26c26b4ec20027667124b6857a9ebbd949
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-03 16:58:02 +0900

    Update answer.php

--- online/talkshow/01/answer.php
+++ online/talkshow/01/answer.php
@@ -25,7 +25,7 @@ fclose($fp);
 <meta name="viewport" content="width=device-width">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="http://ichoose.pe.hu/10q/answer.css" />
+<link rel="stylesheet" type="text/css" href="../answer.css" />
 <link rel="stylesheet" type="text/css" href="http://ichoose.pe.hu/10q/styles.css" />
 <link rel="stylesheet" type="text/css" href="http://ichoose.pe.hu/10q/10q.css" />
 <link rel="stylesheet" type="text/css" href="http://ichoose.pe.hu/10q/print.css" />
