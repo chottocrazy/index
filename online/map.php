ddf2687c2549fb57593d33dbc2248faa0713bcd8
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-03 19:45:06 +0900

    Create map.php

---
+++ online/map.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(function(){
+$("#howtocoding").load("/online/workshop.php");
+})
+</script>
+<style type="text/css">
+@font-face {
+  font-family: "inscrutable";
+  src: url(/font/inscrutable.otf);
+}
+@font-face {
+  font-family: "Orchard";
+  src: url(/font/Orchard-Linear.otf);
+}
+.hold:before,
+.center b,
+#year {
+  font-family:"Orchard";
+}
+.bg_fff,
+.bg_font {
+  font-family:"inscrutable";
+}
+#sign  {
+  width:100%; height:23.5rem;
+  z-index:100;
+  border:none;
+  overflow:hidden;
+}
+#printedweb,
+#howtocoding
+{overflow:auto; padding:0; margin:0; height:23.5rem;}
+</style>
+<link rel="stylesheet" type="text/css" href="/css/map.css" />
+<link rel="stylesheet" type="text/css" href="/css/top.css" />
+<link rel="stylesheet" type="text/css" href="/css/popup.css" />
+<title>オンライン発表 | 大 chotto crazy 2021</title>
+</head>
+<body>
+<div id="full">
+<div id="normal" style="background:#fff;">
+<iframe id="sign" src="/online/about.html"></iframe>
+</div>
+<div id="normal">
+<a id="link" href="http://creative-community.pe.hu/coding/" target="_parent"></a>
+<iframe id="sign" src="/online/marquee.html"></iframe>
+</div>
+<div id="main">
+<div id="howtocoding"></div>
+</div>
+<div id="normal" style="background:#fff;">
+<iframe id="sign" src="http://vg.pe.hu/jp/cm/kaochomex/think/list.php"></iframe>
+</div>
+<div id="normal">
+<iframe id="sign" src="http://newlifecollection.pe.hu/beta/special.php"></iframe>
+</div>
+<div id="main">
+<a id="link" href="http://newlifecollection.pe.hu/" target="_parent"></a>
+<iframe id="sign" src="http://newlifecollection.pe.hu/"></iframe>
+</div>
+<div id="main">
+<div id="freetime"></div>
+</div>
+</div>
+</div>
+</body>
+</html>
