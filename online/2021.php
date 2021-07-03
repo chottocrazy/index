4156891a3330f4cac45a202e6c9ec7565c966815
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-03 22:52:38 +0900

    Update 2021.php

--- online/2021.php
+++ online/2021.php
@@ -47,10 +47,10 @@ hr {clear: both; border: none;}
 <body>
 <div id="full">
 <div id="normal">
-<iframe id="sign" src="/2021/online/matter/"></iframe>
+<iframe id="sign" src="/2021/online/p5/"></iframe>
 </div>
 <div id="normal">
-<iframe id="sign" src="/2021/online/p5/"></iframe>
+<iframe id="sign" src="/online/ichoose/topics.php"></iframe>
 </div>
 <div id="main">
 <iframe id="sign" src="/2021/online/tone/"></iframe>
@@ -63,7 +63,7 @@ hr {clear: both; border: none;}
 <iframe id="sign" src="http://vg.pe.hu/jp/cm/kaochomex/think/list.php"></iframe>
 </div>
 <div id="normal">
-<iframe id="sign" src="/online/ichoose/topics.php"></iframe>
+<iframe id="sign" src="/2021/online/matter/"></iframe>
 </div>
 </div>
 </body>
