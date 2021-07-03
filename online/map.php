affe369aabaff14fa5dc26dc110ec9fe43fd8960
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-03 20:22:57 +0900

    online

--- online/map.php
+++ online/map.php
@@ -58,10 +58,6 @@ hr {clear: both; border: none;}
 </div>
 <hr/>
 <div id="full">
-<div id="normal">
-<a id="link" href="http://creative-community.pe.hu/coding/" target="_parent"></a>
-<iframe id="sign" src="/online/marquee.html"></iframe>
-</div>
 <div id="normal" style="background:#fff;">
 <iframe id="sign" src="http://vg.pe.hu/jp/cm/kaochomex/think/list.php"></iframe>
 </div>
