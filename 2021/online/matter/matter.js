c41dbb3dc0fa761f6ed4ebe711dccdae0c8e58c7
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-08 14:24:49 +0900

    Update matter.js

--- 2021/online/matter/matter.js
+++ 2021/online/matter/matter.js
@@ -196,7 +196,7 @@ function addBlock(){
   World.add(engine.world, [body]);
 
   if(bodies_list.length < words_arr.length){
-      var t = setTimeout(addBlock, 14000);
+      var t = setTimeout(addBlock, 500);
   }
 }
 
