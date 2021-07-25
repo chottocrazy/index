1bcab291a3cd6148a71891f249dab93a99b822f6
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-25 17:54:43 +0900

    Update index.php

--- online/talkshow/00/index.php
+++ online/talkshow/00/index.php
@@ -6,9 +6,27 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" type="text/css" href="../answer.css" />
 <title>10 Questions | by ケニーペイン</title>
+<style type="text/css">
+#center {
+    position:fixed;
+    z-index:0;
+    width:100%; height:100%;
+    top:0; left:0;
+}
+#center img {
+  position: fixed;
+  margin:0; padding:0;
+  -webkit-transform:translate(-50%,-50%);
+  transform:translate(-50%,-50%);
+  width:55%;
+  top: 50%; left: 50%;
+  border-radius:1rem;
+}
+</style>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 </head>
 <body>
+<p id="center"><img src="http://chottocrazy.pe.hu/online/talkshow/image.png"></p>
 <div id="infomation">
 <h3>暗闇でおしゃべりしたい〇〇のこと</h3>
 <p id="next"><a href="../01/">Start</a></p>
