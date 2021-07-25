5aa2eec9751c9d3ace4a26dede0e1af7eda10349
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-25 13:59:08 +0900

    Update complete.php

--- cando/complete.php
+++ cando/complete.php
@@ -33,7 +33,7 @@ fclose($fp);
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta http-equiv="refresh" content="3;URL=submit.php">
+<meta http-equiv="refresh" content="3;URL=index.php">
 <title>大 chotto crazy | 実現したいことを実現する</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="/js/programs.js"></script>
