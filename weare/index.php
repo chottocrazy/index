0d1ca133ba48dd7c14323c5f3b76a1c7881f9512
Kazuma Sasajima <88921218+verygois@users.noreply.github.com>
2021-08-20 16:31:57 +0900

    Update index.php

--- weare/index.php
+++ weare/index.php
@@ -41,7 +41,6 @@ fclose($fp);
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript">
 $(function(){
-$("#form").load("submit.php");
 })
 </script>
 <style type="text/css">
