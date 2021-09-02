b20175617efc8dbbe445ac15e91e65adf99f4cd2
Very Gois <we.are.pe.hu@gmail.com>
2021-09-03 00:10:56 +0900

    Update goout.php

--- update/goout.php
+++ update/goout.php
@@ -73,7 +73,7 @@ $("#").load("");
 <div class="nlc">
 <div id="topics" class="online">
 <div id="about_text">
-<p id="sub">ペフが運営する会員制コミュニティサイト「ニューライフコレクション」のみが参加できる限定イベント</p>
+<p id="sub">会員制コミュニティサイト「ニューライフコレクション」のみが参加できる限定イベント</p>
 </div>
 </div>
 <?php if (!empty($rows)): ?>
